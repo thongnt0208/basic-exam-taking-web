69ca94d6d163ffa6d52aaabf894568545137604e
trungthongnguyen2002@gmail.com <trungthongnguyen2002@gmail.com>
2023-11-01 16:34:14 +0700

    Multiple question fix

--- src/layout/ExamScreen/MakeQuizz/MakeQuizz.js
+++ src/layout/ExamScreen/MakeQuizz/MakeQuizz.js
@@ -5,6 +5,7 @@ import { removeCountdownTime } from "../../../util/CountTime";
 import { SubmitAnswer } from "../ExamService";
 import { useNavigate } from "react-router-dom";
 import { ProgressSpinner } from "primereact/progressspinner";
+import { RadioButton } from "primereact/radiobutton";
 
 export default function MakeQuizz({ quizz }) {
   console.log("quizz-MakeQuiz", quizz);
@@ -19,25 +20,36 @@ export default function MakeQuizz({ quizz }) {
       (item) => item.quesId === questionId && item.answerId === answerId
     );
 
-    if (answerIndex !== -1) {
-      // If the answer is already selected, remove it from selectedAnswers
-      const updatedAnswers = [...selectedAnswers];
-      updatedAnswers.splice(answerIndex, 1);
-      setSelectedAnswers(updatedAnswers);
-      localStorage.setItem("selectedAnswers", JSON.stringify(updatedAnswers));
+    const question = questions.find((q) => q.id === questionId);
+
+    if (question.isMutiple) {
+      // If the question allows multiple answers, toggle the selected state of the answer
+      if (answerIndex !== -1) {
+        // If the answer is already selected, remove it from selectedAnswers
+        const updatedAnswers = [...selectedAnswers];
+        updatedAnswers.splice(answerIndex, 1);
+        setSelectedAnswers(updatedAnswers);
+      } else {
+        // If the answer is not selected, add it to selectedAnswers
+        const newAnswer = {
+          quesId: questionId,
+          answerId: answerId,
+        };
+        setSelectedAnswers([...selectedAnswers, newAnswer]);
+      }
     } else {
-      // If the answer is not selected, add it to selectedAnswers
+      // If the question does not allow multiple answers, replace the existing selected answer with the new one
+      const updatedAnswers = selectedAnswers.filter(
+        (item) => item.quesId !== questionId
+      );
       const newAnswer = {
         quesId: questionId,
         answerId: answerId,
       };
-      setSelectedAnswers([...selectedAnswers, newAnswer]);
-      localStorage.setItem("quizzId", quizz.id);
-      localStorage.setItem(
-        "selectedAnswers",
-        JSON.stringify([...selectedAnswers, newAnswer])
-      );
+      setSelectedAnswers([...updatedAnswers, newAnswer]);
     }
+
+    localStorage.setItem("selectedAnswers", JSON.stringify(selectedAnswers));
   };
 
   let questions = Object.values(quizz.lsQuizz);
@@ -66,23 +78,48 @@ export default function MakeQuizz({ quizz }) {
         <div key={question.id} className="question">
           <h2>{question.content}</h2>
           <ul>
-            {question.isMutiple && (
+            {!question.isMutiple && (
               <p style={{ color: "blue" }}>Please choose only 1 answer</p>
             )}
-            {!question.isMutiple && (
+            {question.isMutiple && (
               <p style={{ color: "blue" }}>
                 Maybe there are more than 1 right answer
               </p>
             )}
             {question.answer.map((answer) => (
               <div key={answer.id}>
-                <Checkbox
-                  checked={selectedAnswers.some(
-                    (item) =>
-                      item.quesId === question.id && item.answerId === answer.id
-                  )}
-                  onChange={() => handleAnswerClick(question.id, answer.id)}
-                />
+                {!question.isMutiple && (
+                  <RadioButton
+                    checked={selectedAnswers.some(
+                      (item) =>
+                        item.quesId === question.id &&
+                        item.answerId === answer.id
+                    )}
+                    onChange={() =>
+                      handleAnswerClick(
+                        question.id,
+                        answer.id,
+                        question.isMutiple
+                      )
+                    }
+                  />
+                )}
+                {question.isMutiple && (
+                  <Checkbox
+                    checked={selectedAnswers.some(
+                      (item) =>
+                        item.quesId === question.id &&
+                        item.answerId === answer.id
+                    )}
+                    onChange={() =>
+                      handleAnswerClick(
+                        question.id,
+                        answer.id,
+                        question.isMutiple
+                      )
+                    }
+                  />
+                )}
                 {answer.content}
               </div>
             ))}
